a397312f212e2299bdae74006e4361e15546eb75
Fernando Morales <fernando.morales@ubiquo.io>
2022-08-03 00:08:04 -0600

    Adding trigonometric functions

--- src/Trigonometricas.java
+++ src/Trigonometricas.java
@@ -2,45 +2,61 @@ import java.util.Scanner;
 
 public class Trigonometricas {
     static Scanner scanner = new Scanner(System.in);
+    static double x;
+    static int n;
     public static void selectTrigonometricas(int selectedOption) {
+        getNAndX();
         switch (selectedOption) {
             case 1:
-                sinWithMaclaurinSeries();
+                System.out.println("sin con math = " + Math.sin(x));
+                System.out.println("sin(x) = " +  sinWithMaclaurinSeries(x, n));
                 break;
             case 2:
-                //  coseno();
+                System.out.println("cos con math = " + Math.cos(x));
+                System.out.println("cos(x) = " + cosWithMaclaurinSeries(x, n));
                 break;
             case 3:
-                //  tangente();
+                System.out.println("Tangente con math" + Math.tan(x));
+                System.out.println("tan(x) = " + tangent(sinWithMaclaurinSeries(x, n), cosWithMaclaurinSeries(x, n)));
                 break;
             default:
                 break;
         }
     }
 
-    private static void sinWithMaclaurinSeries() {
-        System.out.println("Ingresa el valor de x");
-        double x = scanner.nextDouble();
-        double result = 0;
-        double term = 0;
-        double n = 0;
-        double sign = 1;
-        while (Math.abs(term) > 1e-15) {
-            term = Math.pow(x, n) / factorial((int) n);
-            result += term * sign;
-            n++;
-            sign *= -1;
-        }
-        System.out.println("El resultado es: " + result);
+    private static void getNAndX() {
+        System.out.println("Ingrese el valor de x: ");
+        x = scanner.nextDouble();
+        System.out.println("Ingrese el valor de n: ");
+        n = scanner.nextInt();
     }
 
-
-
-    private static int factorial(int n) {
-        if (n == 0) {
-            return 1;
-        } else {
-            return n * factorial(n - 1);
+    private static double sinWithMaclaurinSeries(double x, int n) {
+        double Sum = 0;
+        for (int i = 0; i < n; i++) {
+            Sum += (Helpers.pow(-1, i) * Helpers.pow(x, 2 * i + 1)) / factorial(2 * i + 1);
         }
+        return Sum;
+    }
+
+    private static double cosWithMaclaurinSeries(double x, int n) {
+        double Sum = 0;
+        for (int i = 0; i < n; i++) {
+            Sum += (Helpers.pow(-1, i) * Helpers.pow(x, 2 * i)) / factorial(2 * i);
+        }
+        return Sum;
+    }
+
+    private static double tangent(double sin, double cos) {
+        double result = (sin / cos);
+        return result;
+    }
+
+    static double factorial(int n) {
+        double fact=1;
+        for(int i=2;i<=n;i++)  {
+            fact = fact*i;
+        }
+        return fact;
     }
 }
